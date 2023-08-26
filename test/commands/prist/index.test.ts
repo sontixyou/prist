bda86375ef0cb41c575f7411d33faa1d1bbd0089
kenken <kengoviolinphoto@gmail.com>
2023-08-26 11:23:25 +0900

    replace hello test to prist test

--- test/commands/prist/index.test.ts
+++ test/commands/prist/index.test.ts
@@ -3,7 +3,7 @@ import {expect, test} from '@oclif/test'
 describe('hello', () => {
   test
   .stdout()
-  .command(['hello', 'friend', '--from=oclif'])
+  .command(['prist'])
   .it('runs hello cmd', ctx => {
     expect(ctx.stdout).to.contain('hello friend from oclif!')
   })
